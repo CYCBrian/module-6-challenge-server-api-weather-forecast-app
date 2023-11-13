b1b3b11681f72f43a0486cc41f73246236f0feaf
CYCBrian <143226784+CYCBrian@users.noreply.github.com>
2023-11-13 01:19:01 -0500

    issue 3 - ADDED current weather display

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -9,25 +9,54 @@ function cityWeatherCall() {
     var currentWeatherApi = `https://api.openweathermap.org/data/2.5/weather?q=${cityInputLc}&units=${"metric"}&appid=${apiKey}`
 
     fetch(currentWeatherApi)
+
         .then(response => response.json())
         .then(currentWeatherData => {
         console.log(currentWeatherData);
+
         var cityName = currentWeatherData.name
-        console.log(cityName)
+        console.log(cityName);
+
         var currentIcon = currentWeatherData.weather[0].icon
         var currentIconUrl = `https://openweathermap.org/img/wn/${currentIcon}.png`
-        console.log(currentIconUrl)
-        var currentTemp = currentWeatherData.main.temp
-        console.log(currentTemp)
-        var currentWind = currentWeatherData.wind.speed
-        console.log(currentWind)
-        var currentHumidity = currentWeatherData.main.humidity
-        console.log(currentHumidity)
+        var iconElement = document.createElement("img");
+        iconElement.src = currentIconUrl;
+        console.log(iconElement);
+
+        var currentTemp = currentWeatherData.main.temp;
+        console.log(currentTemp);
+
+        var currentWind = currentWeatherData.wind.speed;
+        console.log(currentWind);
+
+        var currentHumidity = currentWeatherData.main.humidity;
+        console.log(currentHumidity);
+
         var currentDate = new Date(currentWeatherData.dt * 1000);
-        var dateSequence = {month: "long", day:"numeric", year: "numeric"}
-        var formattedCurrentDate = currentDate.toLocaleDateString (undefined, dateSequence)
+        var dateSequence = {month: "long", day:"numeric", year: "numeric"};
+        var formattedCurrentDate = currentDate.toLocaleDateString (undefined, dateSequence);
         console.log(formattedCurrentDate);
 
+        var locationDateIcon = document.querySelector(".location-date-icon");
+        locationDateIcon.textContent = `${cityName} (${formattedCurrentDate})`;
+        locationDateIcon.appendChild(iconElement);
+        
+        var currentSection = document.querySelector(".currentSection");
+        currentSection.innerHTML = "";
+        
+        var tempElement = document.createElement("li");
+        tempElement.textContent = `Temperature: ${currentTemp} °C`;
+        currentSection.appendChild(tempElement);
+        
+        var windElement = document.createElement("li");
+        windElement.textContent = `Wind Speed: ${currentWind} KMPH`;
+        currentSection.appendChild(windElement);
+        
+        var humidityElement = document.createElement("li");
+        humidityElement.textContent = `Relative Humidity: ${currentHumidity} %`;
+        currentSection.appendChild(humidityElement);
+
+        
     });
 
     fetch(forecastWeatherApi)
@@ -41,4 +70,5 @@ function cityWeatherCall() {
 searchBtn.addEventListener('click', function(event) {
     cityWeatherCall()
     locationSearch.value = ""
+
 })
